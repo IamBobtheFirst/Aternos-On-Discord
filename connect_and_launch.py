cbd32b844ffb98de5cf43e02b4c9b1a2d121f149
AAAVPrakash12 <aaavprakash@bob-mac.lan>
2021-07-18 00:38:45 -0500

    max window

--- connect_and_launch.py
+++ connect_and_launch.py
@@ -24,6 +24,7 @@ if headless:
 options.add_argument("window-size=1920x1480")
 options.add_argument("disable-dev-shm-usage")
 options.add_argument("--no-sandbox")
+options.add_argument("start-maximized")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                      "AppleWebKit/537.36 (KHTML, like Gecko) "
                      "Chrome/87.0.4280.88 Safari/537.36")
